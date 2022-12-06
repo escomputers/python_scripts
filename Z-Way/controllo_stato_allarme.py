c3d2927cf497d501d71f02ec1e3912f9ce8ce345
escomputers <emilianos13@gmail.com>
2022-12-06 20:14:42 +0100

    Update controllo_stato_allarme.py

--- Z-Way/controllo_stato_allarme.py
+++ Z-Way/controllo_stato_allarme.py
@@ -2,11 +2,8 @@ import os
 import json
 import requests
 import threading
-import pytz
 from datetime import datetime, timedelta
 
-# imposta fuso orario
-tmzone = pytz.timezone('Europe/Rome')
 
 # imposta ora inizio
 alarmStartTime = datetime.strptime('21:30', '%H:%M')
@@ -20,14 +17,14 @@ def allarme_on():
     last_state = (res.get('value'))
     if last_state == 0:
         on_url = 'http://localhost:8083/ZAutomation/api/v1/devices/DummyDevice_20/command/on'
-        http_post = requests.get(on_url)
+        http_on = requests.get(on_url)
 
 
 def main():
     threading.Timer(3.0, main).start()
 
     # calcola orario di attivazione e disattivazione
-    now = (datetime.now(tmzone))
+    now = datetime.now()
     start = now.replace(hour=alarmStartTime.hour, minute=alarmStartTime.minute)
     end = (start + timedelta(hours=11))
 
@@ -39,17 +36,13 @@ def main():
 
         # rimuovi spazi da stringa e converti a oggetto datetime
         cmd_output_cleaned = cmd_output.strip()
-        dt_last_reboot = datetime.strptime(cmd_output_cleaned, '%Y-%m-%d %H:%M:%S')
-
-        # aggiungi fuso orario all'orario dell'ultimo reboot
-        last_reboot = dt_last_reboot.replace(tzinfo=tmzone)
+        last_reboot = datetime.strptime(cmd_output_cleaned, '%Y-%m-%d %H:%M:%S')
 
         # estrai ore e minuti da oggetto timedelta
-        hour_time_diff = (now - last_reboot).seconds//3600
         minutes_time_diff = ((now - last_reboot).seconds//60)%60
 
-        # se sono passati due minuti dall'ultimo reboot
-        if hour_time_diff == 0 and minutes_time_diff >= 2:
+        # se sono passati più di due minuti dall'ultimo reboot
+        if minutes_time_diff >= 2:
             allarme_on()
 
 
